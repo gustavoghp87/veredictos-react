3e0a64043d6ad86d3ea1d6cc0c7773048939f599
Gustavo Ghp <ghp.21@hotmail.com>
2022-05-11 17:24:11 -0300

    Github page

---
+++ docs/precache-manifest.bdd7613dd63f667af8304cc3645324c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93fc1bd0af299406d74b612b09a227f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da66b334ec8feba36d5d",
+    "url": "/static/js/2.42a64e56.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.42a64e56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74c052b97688c30f584f",
+    "url": "/static/js/main.6a570b4a.chunk.js"
+  },
+  {
+    "revision": "a7ed49496b1b4b1f30b9",
+    "url": "/static/js/runtime-main.d9473891.js"
+  }
+]);
